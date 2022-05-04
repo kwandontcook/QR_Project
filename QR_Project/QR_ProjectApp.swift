643739ac9ec8d08e132ffccf23627d9366ddbfbb
kwandontcook <kwandontcook@gmail.com>
2022-05-04 13:18:52 +0930

    Initial Commit

---
+++ QR_Project/QR_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  QR_ProjectApp.swift
+//  QR_Project
+//
+//  Created by kwok chung  kwan on 4/5/2022.
+//
+
+import SwiftUI
+
+@main
+struct QR_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ QR_ProjectUITests/QR_ProjectUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  QR_ProjectUITestsLaunchTests.swift
+//  QR_ProjectUITests
+//
+//  Created by kwok chung  kwan on 4/5/2022.
+//
+
+import XCTest
+
+class QR_ProjectUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
